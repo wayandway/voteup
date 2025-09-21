3d55bd67c1d7d5a6c92f6711f62b3740617b2d0b
Jiyun Kim <wayandway@kakao.com>
2025-09-22 04:43:33 +0900

    chore: 프로젝트 설정 및 의존성 업데이트
    - supabase 폴더 gitignore 추가
    - Next.js 로컬 스토리지 이미지 허용 설정
    - @radix-ui/react-slider 의존성 추가

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "54321",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "54321",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
